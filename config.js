a18abb973bfbb2fd55cbad299b650255d0e09048
madhavgupta19 <madhavgupta3210@gmail.com>
2025-03-26 17:53:05 +0530

    Update in config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD
+require('dotenv').config();
+
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
 
 module.exports = {
